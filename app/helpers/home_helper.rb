8b3f1600238ee1d7ee8cb9175fcb14b61df90d2a
タクロ <ty.tf.pmsw@gmail.com>
2023-12-31 13:20:32 +0900

    sass導入

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,26 @@
 module HomeHelper
+    def page_items
+        [
+            {
+                name: "Home",
+                path: root_path
+            },
+            {
+                name: "About",
+                path: about_path
+            },
+            {
+                name: "Contact",
+                path: contact_path
+            },
+            {
+                name: "Blog",
+                path: blog_path
+            },
+            {
+                name: "Portfolio",
+                path: portfolios_path
+            }
+        ]
+    end
 end
