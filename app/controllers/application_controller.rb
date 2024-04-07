a8b3ed644a519590ef82047946ebb1aefbb2a122
タクロ <ty.tf.pmsw@gmail.com>
2024-04-07 18:41:16 +0900

    staging稼動用に調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    if Rails.env.staging?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == Rails.application.credentials.basic_auth[:name] && password == Rails.application.credentials.basic_auth[:password]
+      end
+    end
+  end
 end
