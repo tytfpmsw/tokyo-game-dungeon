2e21c350c057f1a81c7a4efeb4df5831e40c0a0e
タクロ <ty.tf.pmsw@gmail.com>
2024-04-19 22:43:29 +0900

    Eventにscopeを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,27 +1,31 @@
 class Event < ApplicationRecord
-    has_many :exhibit_informations
-    has_many :sponsorships, dependent: :destroy
-    has_many :sponsors, through: :sponsorships
-    has_many :floors, dependent: :destroy
+  has_many :exhibit_informations
+  has_many :sponsorships, dependent: :destroy
+  has_many :sponsors, through: :sponsorships
+  has_many :event_schedule, dependent: :destroy
 
-    validates :name, presence: true
-    validates :url_subdirectory, presence: true, uniqueness: true, format: { with: /\A[a-z0-9_\-]+\z/ }
-    validates :location, presence: true
+  validates :name, presence: true
+  validates :url_subdirectory, presence: true, uniqueness: true, format: { with: /\A[a-z0-9_\-]+\z/ }
+  validates :location, presence: true
 
-    mount_uploader :logo_image, EventLogoImageUploader
-    mount_uploader :main_image, EventMainImageUploader
-    mount_uploader :reflection_image, ReflectionImageUploader
+  validate :exhibit_submit_period_is_valid, if: -> { exhibit_submit_start_at.present? && exhibit_submit_end_at.present? }
 
-    enum :status, { unpublished: 0, published: 1, archived: 2 }, _prefix: true
-    enum :location, { undecided: 0, hamamatsu_tsbc: 1 }, _prefix: true
+  mount_uploader :logo_image, EventLogoImageUploader
+  mount_uploader :main_image, EventMainImageUploader
+  mount_uploader :reflection_image, ReflectionImageUploader
 
-    scope :accepting_submissions, -> { where(status: :accepting_submissions) }
+  enum :status, { unpublished: 0, published: 1, archived: 2 }, _prefix: true
+  enum :location, { undecided: 0, hamamatsu_tsbc: 1 }, _prefix: true
 
-    def accepting_submissions?
-        status == 'accepting_submissions'
-    end
-
-    def closed?
-        status == 'closed'
+  scope :first_day_asc, -> { joins(:event_schedule).order(Arel.sql('event_schedules.start_at')) }
+  scope :first_day_desc, -> { joins(:event_schedule).order(Arel.sql('event_schedules.start_at DESC')) }
+  scope :published_event_date_asc, -> { where(status: :published).first_day_asc }
+  scope :archived_event_date_desc, -> { where(status: :archived).first_day_desc }
+  
+  private
+    def exhibit_submit_period_is_valid
+      if exhibit_submit_start_at >= exhibit_submit_end_at
+        errors.add(:exhibit_submit_start_at, :invalid)
+      end  
     end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,67 @@
 require "test_helper"
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @event = events(:now_preparing)
+  end
+
+  test "should not save event without name" do
+    event = Event.new(url_subdirectory: "test", location: 0)
+    assert_not event.save, "Saved the event without a name"
+  end
+
+  test "should not save event without url_subdirectory" do
+    event = Event.new(name: "test", location: 0)
+    assert_not event.save, "Saved the event without a url_subdirectory"
+  end
+
+  test "should save event with name, url_subdirectory, and location" do
+    event = Event.new(name: "test", url_subdirectory: "test", location: 0)
+    assert event.save, "Did not save the event with name, url_subdirectory, and location"
+  end
+
+  test "should not save event with duplicate url_subdirectory" do
+    event = Event.new(name: "test", url_subdirectory: @event.url_subdirectory, location: 0)
+    assert_not event.save, "Saved the event with a duplicate url_subdirectory"
+  end
+
+  test "should not save event with invalid url_subdirectory" do
+    event = Event.new(name: "test", url_subdirectory: "test!", location: 0)
+    assert_not event.save, "Saved the event with an invalid url_subdirectory"
+  end
+
+  test "should save event with valid url_subdirectory" do
+    event = Event.new(name: "test", url_subdirectory: "aaaa-_aa", location: 0)
+    assert event.save, "Did not save the event with a valid url_subdirectory"
+  end
+
+  test "should save event with valid location" do
+    event = Event.new(name: "test", url_subdirectory: "test", location: 0)
+    assert event.save, "Did not save the event with a valid location"
+  end
+
+  test "should not save event when exhibit submit period is invalid" do
+    event = Event.new(
+      name: "test",
+      url_subdirectory: "test",
+      location: 0,
+      exhibit_submit_start_at: Time.zone.now,
+      exhibit_submit_end_at: Time.zone.now - 1.day
+    )
+    assert_not event.save, "Saved the event when exhibit submit period is invalid"
+  end
+
+  test "should get published events in ascending order of event date" do
+    events = Event.published_event_date_asc
+    assert_equal 2, events.count, "Did not get published events in ascending order of event date"
+    assert_equal events[0], events(:now_preparing), "Did not get published events in ascending order of event date"
+    assert_equal events[1], events(:future), "Did not get published events in ascending order of event date"
+  end
+
+  test "should get archived events in descending order of event date" do
+    events = Event.archived_event_date_desc
+    assert_equal 1, events.count, "Did not get archived events in descending order of event date"
+    assert_equal events[0], events(:past), "Did not get archived events in descending order of event date"
+  end
 end
