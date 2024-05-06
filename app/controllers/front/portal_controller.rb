2cd3bdb8821c9f4df9ceae0bbc88435be506217b
タクロ <ty.tf.pmsw@gmail.com>
2024-05-06 14:58:52 +0900

    frontポータルページのイベントの表示制御を追加

--- app/controllers/front/portal_controller.rb
+++ app/controllers/front/portal_controller.rb
@@ -1,5 +1,6 @@
 class Front::PortalController < ApplicationController
   def index
-    @events = Event.all
+    @events_upcoming = Event.published_event_date_asc
+    @events_archived = Event.archived_event_date_desc.limit(3)
   end
 end
