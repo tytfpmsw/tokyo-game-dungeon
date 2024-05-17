b8c20337dd1628bd9dbe81ebd7d2d7b839065fe0
タクロ <ty.tf.pmsw@gmail.com>
2024-05-17 22:21:57 +0900

    recaptchaのやり方を変更

--- app/controllers/front/events_controller.rb
+++ app/controllers/front/events_controller.rb
@@ -7,7 +7,9 @@ class Front::EventsController < ApplicationController
 
   def inquiry
     @event = Event.find_by!(url_subdirectory: params[:event_url_subdirectory])
-    if verify_recaptcha(action: 'inquiry', minimum_score: 0.5)
+    success = verify_recaptcha(action: 'inquiry', minimum_score: 0.5)
+    checkbox_success = verify_recaptcha unless success
+    if success || checkbox_success
       name = params[:name]
       email = params[:email]
       content = params[:content]
@@ -15,8 +17,10 @@ class Front::EventsController < ApplicationController
       flash[:notice] = "お問い合わせを受け付けました。"
       redirect_to front_root_path
     else
-      flash[:alert] = "reCAPTCHAによる認証に失敗しました。時間をおいて再度お試しください。"
-      redirect_to front_root_path
+      if !success
+        @show_checkbox_recaptcha = true
+      end
+      render :show
     end
   end
 end
