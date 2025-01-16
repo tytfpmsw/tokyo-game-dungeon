f56f2e174e81dbb70d2d18416c91196986d65056
タクロ <ty.tf.pmsw@gmail.com>
2025-01-16 20:57:38 +0900

    ポータルのイベント一覧に複数開催日のイベントが開催日数表示されてしまう問題の修正

--- app/controllers/front/portal_controller.rb
+++ app/controllers/front/portal_controller.rb
@@ -1,6 +1,6 @@
 class Front::PortalController < ApplicationController
   def index
-    @events_upcoming = Event.published_event_date_asc
-    @events_archived = Event.archived_event_date_desc.limit(3)
+    @events_upcoming = Event.published
+    @events_archived = Event.archived.limit(3)
   end
 end
